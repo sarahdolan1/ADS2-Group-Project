03ab3f342c0eb36e15bf30518ec9b2bec0e415b0
Sean <78490372+SeanLangan@users.noreply.github.com>
2021-04-15 13:54:21 +0100

    Add files via upload
    findShortestPath class. First test commit

---
+++ findShortestPath.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class findShortestPath {
+	 
+	//TODO Find Shortest Path Method
+	/*
+	 * findShortestPath(String stop1,String stop2)
+	 * 
+	 * return String stopsAlongWay[], int  numberOfStops
+	 * 
+	 * stop1, stop2 input by the user.
+	 * 
+	 */
+}
